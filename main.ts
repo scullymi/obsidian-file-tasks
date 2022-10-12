fcba6e509024ee9ce9e492c035d929aa7cef8125
Alexander Hach <git@twork.de>
2022-10-12 09:25:28 +0200

    rename and clear samples

--- main.ts
+++ main.ts
@@ -2,33 +2,40 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface FileTasksPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: FileTasksPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FileTasksPlugin extends Plugin {
+	settings: FileTasksPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
+
+
 		// This creates an icon in the left ribbon.
+		/*
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		*/
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		/*
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('File Tasks');
+		*/
 
 		// This adds a simple command that can be triggered anywhere
+		/*
 		this.addCommand({
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
@@ -36,7 +43,9 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
+		*/
 		// This adds an editor command that can perform some operation on the current editor instance
+		/*
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -45,7 +54,9 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
+		*/
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		/*
 		this.addCommand({
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
@@ -64,18 +75,24 @@ export default class MyPlugin extends Plugin {
 				}
 			}
 		});
+		*/
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new FileTasksSettingTab(this.app, this));
+		
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
+		/*
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
+		/*
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		/*
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		*/
 	}
 
 	onunload() {
@@ -91,6 +108,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
+/*
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
@@ -106,11 +124,11 @@ class SampleModal extends Modal {
 		contentEl.empty();
 	}
 }
+*/
+class FileTasksSettingTab extends PluginSettingTab {
+	plugin: FileTasksPlugin;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FileTasksPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
